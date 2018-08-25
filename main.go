79c08d02bec315f8a832b74a731869ec8954dfc0
Steve Hillier <stephenhillier@gmail.com>
2018-08-24 21:40:49 -0700

    add shutdown logic when receiving interrupt signal

--- main.go
+++ main.go
@@ -1,19 +1,40 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
 )
 
 func main() {
 
 	port := 8000
 
-	log.Printf("Starting HTTP server on port %v.", port)
-	log.Println("Press CTRL+C to stop.")
-	http.HandleFunc("/health", health)
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	// add a channel that accepts an interrupt signal
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+
+	server := http.Server{Addr: fmt.Sprintf(":%v", port), Handler: nil}
+
+	// start server and continue
+	go func() {
+		log.Printf("Starting HTTP server on port %v.", port)
+		log.Println("Press CTRL+C to stop.")
+		http.HandleFunc("/health", health)
+		log.Fatal(server.ListenAndServe())
+	}()
+
+	// wait for interrupt signal
+	<-stop
+
+	// interrupt signal received- shut down http server.
+	log.Println("Stopping server.")
+	server.Shutdown(context.Background())
+
 }
 
 func health(w http.ResponseWriter, req *http.Request) {
